f7e4736bc39b2a8d81a58c7d089f71f588369589
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-13 16:59:25 +0900

    [feat] components 🆕 컴포넌트 생성 추후 라우터로 연결하기

---
+++ src/router/RouterPage.js
@@ -0,0 +1,24 @@
+// import NotFound from '../component/NotFound';
+
+
+
+
+
+// export default function RouterPage() {
+//   return (
+//     <Routes>
+//       {/* <Route /> 화면에 노출될 페이지들의 목록 */}
+//       <Route path="" element={<Main />} />
+//       <Route path="/" element={<Main />} />
+//       <Route path="/main" element={<Main />} />
+//       <Route path="/review" element={<Review />} />
+//       <Route path="/cardList" element={<CardList />} />
+//       <Route path="/manual" element={<Manual />} />
+//       <Route path="/person" element={<Person />} />
+//       <Route path="/rule" element={<Rule />} />
+//       <Route path="/product" element={<Product />} />
+//       <Route path="/event" element={<Event />} />
+//       <Route path="*" element={<NotFound />} />
+//     </Routes>
+//   )
+// }
