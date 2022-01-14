b32d662d4e28dec2465a987faf880e6b1fc502bb
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-14 15:50:12 +0900

    [feat] components 🆕sub배너 컨포넌트  생성 및 스타일적용

---
+++ src/components/SubBanner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../style/SubBanner.scss';
+// import subImg from '../image/5e428ed460ffc3881382.png'
+
+
+export default function SubBanner() {
+  return (
+    <article id="subViewBox">
+      <h2 className="blind">기획전</h2>
+      <div className="container">
+        <Link to="/"><img src= "/assets/banner/sample_sub_banner.jpg" alt="2022년 기획전"/></Link>
+
+        {/* <Link to="/"><img src= {subImg}alt="2022년 기획전"/></Link>  */ }
+      </div>
+    </article>
+  )
+}
