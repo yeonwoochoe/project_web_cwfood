d09e09556124705af6cf6275b80ea0f9ed821083
YEONWOO <89890240+yeonwoochoe@users.noreply.github.com>
2022-01-14 15:48:04 +0900

    [feat] components 🆕 NewArribal 영역과 Li  분리 및 카드리스트 생성

--- src/components/NewArrival.js
+++ src/components/NewArrival.js
@@ -1,46 +1,35 @@
 
-import React from 'react'
+import React,  { useEffect, useState }  from 'react';
+import NewArrivalList from './NewArrivalList';
+import axios from 'axios';
 import '../style/NewArrival.scss';
 
+
 export default function NewArrival() {
+  const dataUrl = './data/NewPdList.json';
+  const [dataList, setDataList] = useState([]);
+
+  // data 불러오기
+  useEffect(()=>{
+    // axios.get(dataUrl).then(res => setDataList(res.data) )
+    (async ()=>{
+      const res = await axios.get(dataUrl);
+          setDataList(res.data);
+    })()
+  }, [])
+
+
+
   return (
     <article id="newPdBox">
       <h2 className="blind">신상품</h2>
       <div className="main_title">
         <h3><span>New Arrival</span>
-         새로운 맛 신상품
+         새로운 맛 <strong>신상품</strong>
         </h3>
         </div>
         <ul className="pd_list">
-          <li>
-            <div className="thumbnail">
-              <div className="promotion"></div>
-              <div className="img"> <div/>
-              <div className="shop_bag_icon"></div>
-            </div>
-            </div>
-            <div class="description">
-              <div class="best_pd_name">
-                <a href="/">[청우식픔] 찰떡 초코파이</a>
-              </div>
-              <div class="best_pd_price">
-              <span class="best_pd_sale_price">
-                <span class="blind">할인률</span>
-               20&#37;
-              </span>
-                <span class="best_pd_p_dc">
-                <span class="blind">할인가</span>
-                8,900원
-              </span>
-              <span class="best_pd_per_price">
-                <span class="blind">정가</span>
-               9,500원
-              </span>
-
-
-              </div>
-            </div>
-          </li>
+        { dataList.map( (product,idx)=> <NewArrivalList  key={idx} data={product} />  )}      
         </ul>
   
     </article>
